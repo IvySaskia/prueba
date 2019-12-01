bd39c7c3beef72d1bddc168e422b3672f41b381b
IvySaskia <saskiaivy@outlook.com>
2019-11-30 22:57:20 -0400

    define routes home and default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path:'users', component:UsersComponent}
+  {path:'users', component:UsersComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
